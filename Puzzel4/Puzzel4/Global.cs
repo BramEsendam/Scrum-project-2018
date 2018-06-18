1aa9e659ddf874bc20ac22b5368ff680f83f18a2
chantalwiegand <chantal.wiegand@student.graafschapcollege.nl>
2018-06-18 17:51:11 +0200

    Revert "Merge branch 'master' of https://github.com/BramEsendam/Scrum-project-2018"
    This reverts commit 38d901fb75d611ec71426a596bf71384b246370e, reversing
    changes made to 0856f5969f4d02728b066fb2c8f0ed87726dd191.

--- Puzzel4/Puzzel4/Global.cs
+++ Puzzel4/Puzzel4/Global.cs
@@ -8,30 +8,11 @@ namespace Puzzel4
 {
     internal static class Global
     {
-        internal static string[] Words = {"jupiter", "saturn", "uranus", "neptune", "earth", "venus", "mars", "mercury", "pluto" };
-        internal static Random Rnd = new Random();
-        internal static bool CheckWordInput(int index, string input)
-        {
-            return Words[index].ToLower().Equals(input.ToLower());
-        }
+        internal static string[] Words = {"Milky Way", "Whirlpool Galaxy", "Andromeda Galaxy" };
 
-        internal static bool CheckArrayInput(int index, string input)
+        internal static bool CheckInput(byte index, string input)
         {
-            return !Words[index].ToLower().Equals(input.ToLower());
-        }
-
-        internal static string RandomizeChars(string word)
-        {
-            var chars = new char[word.Length];
-            var index = 0;
-            while (word.Length > 0)
-            {
-                var next = Rnd.Next(0, word.Length - 1);
-                chars[index] = word[next];
-                word = word.Substring(0, next) + word.Substring(next + 1);
-                ++index;
-            }
-            return new string(chars);
+            return Words[index - 1].ToLower().Equals(input.ToLower());
         }
     }
 }
