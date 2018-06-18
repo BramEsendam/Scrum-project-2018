dfd1fcbae01353fa39da67d198c4aa5133bad279
Bram Esendam <bram.esendam@gmail.com>
2018-06-18 09:23:12 +0200

    Added JFram & GitIgnore & added build.xml

---
+++ Snake/src/snake/Paneel.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package snake;
+
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import javax.swing.JPanel;
+
+/**
+ *
+ * @author Bram Esendam
+ */
+public class Paneel extends JPanel implements KeyListener
+{
+
+    public Paneel()
+    {
+
+    }
+
+    @Override
+    public void keyTyped(KeyEvent ke)
+    {
+
+    }
+
+    @Override
+    public void keyPressed(KeyEvent ke)
+    {
+
+    }
+
+    @Override
+    public void keyReleased(KeyEvent ke)
+    {
+
+    }
+
+}
