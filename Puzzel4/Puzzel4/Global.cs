7728b58f1f3ad4cfba6ab89cbaf96865e0b59318
Bram Esendam <bram.esendam@gmail.com>
2018-06-18 13:19:12 +0200

    Added word scrambling

--- Puzzel4/Puzzel4/Global.cs
+++ Puzzel4/Puzzel4/Global.cs
@@ -8,11 +8,25 @@ namespace Puzzel4
 {
     internal static class Global
     {
-        internal static string[] Words = {"Milky Way", "Whirlpool Galaxy", "Andromeda Galaxy" };
-
-        internal static bool CheckInput(byte index, string input)
+        internal static string[] Words = {"saturn", "jupiter", "earth", "uranus", "mercury", "mars", "venus", "neptune"};
+        internal static Random Rnd = new Random();
+        internal static bool CheckInput(int index, string input)
         {
-            return Words[index - 1].ToLower().Equals(input.ToLower());
+            return Words[index].ToLower().Equals(input.ToLower());
+        }
+
+        internal static string RandomizeChars(string word)
+        {
+            var chars = new char[word.Length];
+            var index = 0;
+            while (word.Length > 0)
+            {
+                var next = Rnd.Next(0, word.Length - 1);
+                chars[index] = word[next];
+                word = word.Substring(0, next) + word.Substring(next + 1);
+                ++index;
+            }
+            return new string(chars);
         }
     }
 }
--- Puzzel4/Puzzel4/Program.cs
+++ Puzzel4/Puzzel4/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Puzzel4;
 
 namespace Puzzel4
 {
@@ -17,20 +18,15 @@ namespace Puzzel4
             Console.ForegroundColor = ConsoleColor.Blue;
             Console.WriteLine("Pls put the letters in the correct order to create 2 words.\r\nNote: the theme is space.\r\n");
 
-            Console.WriteLine("Word 1: kmily ywa");
-            //checking if input word is the correct word. If its not the correct word write That's incorrect to the console
-            while (!Global.CheckInput(1, Console.ReadLine())) Console.WriteLine("That's incorrect!");
-            Console.WriteLine("That's correct!\r\n");
 
-            Console.WriteLine("Word 2: lwhiproo ygaaxl");
-            //checking if input word is the correct word. If its not the correct word write That's incorrect to the console
-            while (!Global.CheckInput(2, Console.ReadLine())) Console.WriteLine("That's incorrect!");
-            Console.WriteLine("That's correct!\r\n");
 
-            Console.WriteLine("Word 3: oameddrna laayxg");
-            //checking if input word is the correct word. If its not the correct word write That's incorrect to the console
-            while (!Global.CheckInput(3, Console.ReadLine())) Console.WriteLine("That's incorrect!");
-            Console.WriteLine("That's correct!\r\n");
+            for (var i = 0; i < Global.Words.Length; i++)
+            {
+                Console.WriteLine($"Word {i + 1}: {Global.RandomizeChars(Global.Words[i])}");
+                //checking if input word is the correct word. If its not the correct word write That's incorrect to the console
+                while (!Global.CheckInput(i, Console.ReadLine())) Console.WriteLine("That's incorrect!");
+                Console.WriteLine("That's correct!\r\n");
+            }
 
             //writing the number to console
             Console.BackgroundColor = ConsoleColor.Green;
