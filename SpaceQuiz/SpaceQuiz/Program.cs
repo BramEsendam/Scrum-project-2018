45198e48682f5a786961091b37f49784d29668f3
Biesono <jaccobiezeman@gmail.com>
2018-06-19 12:53:58 +0200

    updated quiz

--- SpaceQuiz/SpaceQuiz/Program.cs
+++ SpaceQuiz/SpaceQuiz/Program.cs
@@ -10,22 +10,34 @@ namespace SpaceQuiz
     {
         static void Main(string[] args)
         {
-            int score = 0;
-
             Console.ForegroundColor = ConsoleColor.Blue;
-            Console.WriteLine("Welcome in the space quiz \nAhnswer with capitol pls \n \n ");
+           // Console.WriteLine("Welcome in the space quiz \n]Answer with capitol pls \n \nPress enter to start");
 
+            quiz();
+            Console.WriteLine("Press r to restart");
+            while (true)
+            {
+                while (Console.ReadKey().Equals(ConsoleKey.Enter))
+                {
+                    quiz();
+                }
+            }
+        }
+
+        public static void quiz()
+        {
+            int score = 0;
             Console.WriteLine("How many planets are in the Solar System?");
             var entry1 = Console.ReadLine();
 
             if (entry1 == "8")
             {
                 score += 1;
-                Console.WriteLine("Correct!!!");
+                Console.WriteLine("Correct!!!  \n");
             }
             else
             {
-                Console.WriteLine("WRONG");
+                Console.WriteLine("WRONG  \n");
             }
 
             Console.WriteLine("What is the smallest planet in our solar system?");
@@ -34,11 +46,11 @@ namespace SpaceQuiz
             if (entry2 == "Mercury")
             {
                 score += 1;
-                Console.WriteLine("Correct!!!");
+                Console.WriteLine("Correct!!!  \n");
             }
             else
             {
-                Console.WriteLine("WRONG");
+                Console.WriteLine("WRONG  \n");
             }
 
             Console.WriteLine("What has a gravitational pull so strong that even light cannot escape it?");
@@ -47,11 +59,11 @@ namespace SpaceQuiz
             if (entry3 == "Black Hole")
             {
                 score += 1;
-                Console.WriteLine("Correct!!!");
+                Console.WriteLine("Correct!!!  \n");
             }
             else
             {
-                Console.WriteLine("WRONG");
+                Console.WriteLine("WRONG  \n");
             }
 
             Console.WriteLine("Who was the first person to walk on the moon?");
@@ -60,11 +72,11 @@ namespace SpaceQuiz
             if (entry4 == "Neil Armstrong")
             {
                 score += 1;
-                Console.WriteLine("Correct!!!");
+                Console.WriteLine("Correct!!!  \n");
             }
             else
             {
-                Console.WriteLine("WRONG");
+                Console.WriteLine("WRONG  \n");
             }
 
             Console.WriteLine("What is the hottest planet in our solar system?");
@@ -73,11 +85,11 @@ namespace SpaceQuiz
             if (entry5 == "Venus")
             {
                 score += 1;
-                Console.WriteLine("Correct!!!");
+                Console.WriteLine("Correct!!!  \n");
             }
             else
             {
-                Console.WriteLine("WRONG");
+                Console.WriteLine("WRONG  \n");
             }
 
             if (score == 5)
@@ -88,7 +100,6 @@ namespace SpaceQuiz
             {
                 Console.WriteLine("pls start again your score was " + score);
             }
-            Console.ReadLine();
         }
     }
 }
