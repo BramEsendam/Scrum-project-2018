d306d07fa6b447ce79a1dec84e865d28f5d4f6c0
Biesono <jaccobiezeman@gmail.com>
2018-06-19 10:45:24 +0200

    1st version of quiz

--- SpaceQuiz/SpaceQuiz/Program.cs
+++ SpaceQuiz/SpaceQuiz/Program.cs
@@ -10,6 +10,86 @@ namespace SpaceQuiz
     {
         static void Main(string[] args)
         {
+            int score = 0;
+
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine("Welcome in the space quiz \nAhnswer with capitol pls \n \n ");
+
+            Console.WriteLine("How many planets are in the Solar System?");
+            var entry1 = Console.ReadLine();
+
+            if (entry1 == "8")
+            {
+                score += 1;
+                Console.WriteLine("Correct!!!");
+            }
+            else
+            {
+                Console.WriteLine("WRONG");
+            }
+
+            Console.WriteLine("What is the smallest planet in our solar system?");
+            var entry2 = Console.ReadLine();
+
+            if (entry2 == "Mercury")
+            {
+                score += 1;
+                Console.WriteLine("Correct!!!");
+            }
+            else
+            {
+                Console.WriteLine("WRONG");
+            }
+
+            Console.WriteLine("What has a gravitational pull so strong that even light cannot escape it?");
+            var entry3 = Console.ReadLine();
+
+            if (entry3 == "Black Hole")
+            {
+                score += 1;
+                Console.WriteLine("Correct!!!");
+            }
+            else
+            {
+                Console.WriteLine("WRONG");
+            }
+
+            Console.WriteLine("Who was the first person to walk on the moon?");
+            var entry4 = Console.ReadLine();
+
+            if (entry4 == "Neil Armstrong")
+            {
+                score += 1;
+                Console.WriteLine("Correct!!!");
+            }
+            else
+            {
+                Console.WriteLine("WRONG");
+            }
+
+            Console.WriteLine("What is the hottest planet in our solar system?");
+            var entry5 = Console.ReadLine();
+
+            if (entry5 == "Venus")
+            {
+                score += 1;
+                Console.WriteLine("Correct!!!");
+            }
+            else
+            {
+                Console.WriteLine("WRONG");
+            }
+
+            if (score == 5)
+            {
+                Console.WriteLine("Nice! you have won pls continue \nyour code is 13");
+            }
+            else
+            {
+                Console.WriteLine("pls start again your score was " + score);
+            }
+            Console.ReadLine();
         }
     }
 }
+
